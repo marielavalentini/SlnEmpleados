71e9b9b02fafe2149c688e88ff90662f6f839ba3
Mariela Valentini <marielavalentini@outlook.com>
2022-11-11 17:47:23 -0300

    Base de datos

---
+++ WindowsEmpleados/Data/DBEmpleadosContext.cs
@@ -0,0 +1,18 @@
+﻿using LibreriaEmpleado;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsEmpleados.Data
+{
+    public class DBEmpleadosContext : DbContext
+    {
+        public DBEmpleadosContext(): base("keyEmpleados") { }
+
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
---
+++ WindowsEmpleados/Migrations/202211112045599_CrearBase.cs
@@ -0,0 +1,43 @@
+﻿namespace WindowsEmpleados.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CrearBase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departamentoes",
+                c => new
+                    {
+                        DepartamentoId = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                    })
+                .PrimaryKey(t => t.DepartamentoId);
+            
+            CreateTable(
+                "dbo.Empleadoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Apellido = c.String(),
+                        Legajo = c.Int(nullable: false),
+                        Departamento_DepartamentoId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Departamentoes", t => t.Departamento_DepartamentoId)
+                .Index(t => t.Departamento_DepartamentoId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Empleadoes", "Departamento_DepartamentoId", "dbo.Departamentoes");
+            DropIndex("dbo.Empleadoes", new[] { "Departamento_DepartamentoId" });
+            DropTable("dbo.Empleadoes");
+            DropTable("dbo.Departamentoes");
+        }
+    }
+}
